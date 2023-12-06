4ba216b79015fd75110164a2da33379f77f5e961
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2023-12-06 17:34:27 +0530

    first commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,68 @@
+function bigScreens() {
+	const signUpButton = document.getElementById('signUp');
+	const signInButton = document.getElementById('signIn');
+	const container = document.getElementById('container');
+	signUpButton.addEventListener('click', () => {
+		container.classList.add('right-panel-active');
+	});
+	signInButton.addEventListener('click', () => {
+		container.classList.remove('right-panel-active');
+	});
+}
+
+function mobileScreens() {
+	const signUpButton = document.getElementById('signUp');
+	const signInButton = document.getElementById('signIn');
+	const container = document.getElementById('container');
+	signUpButton.addEventListener('click', () => {
+		container.classList.add('mobile-right-panel-active');
+	});
+	signInButton.addEventListener('click', () => {
+		container.classList.remove('mobile-right-panel-active');
+	});
+}
+
+
+
+function dyanamicResponsive() {
+	var widthh = window.innerWidth;
+	window.addEventListener('resize', function () {
+		if (window.innerWidth !== widthh) {
+			window.location.reload(true);
+		}
+	});
+	console.log(widthh);
+
+	if (widthh >= 0 && widthh <= 375) {
+		// Code to execute when yourValue is between 0 and 400
+		console.log('/* Media Query for low resolution Mobile Devices */');
+        mobileScreens();
+		console.log('yeee ahi');
+	} else if (widthh >= 376 && widthh <= 600) {
+		// Code to execute when yourValue is between 401 and 600
+		console.log('/* Media Query for Mobile Devices */');
+	} else if (widthh >= 601 && widthh <= 767) {
+		/* Media Query for low resolution  Tablets, Ipads */
+		console.log('Media Query for low resolution  Tablets, Ipads');
+	} else if (widthh >= 768 && widthh <= 1023) {
+		/* Media Query for Tablets Ipads portrait mode */
+		console.log('Media Query for Tablets Ipads portrait mode ');
+	} else if (widthh >= 1024 && widthh <= 1500) {
+		console.log(' /* Media Query for Tab LandScape */');
+		console.log('ye 1024 se upar');
+        bigScreens();
+	}
+}
+
+dyanamicResponsive();
+
+
+document.addEventListener('focusin', function (event) {
+	if (event.target.tagName === 'INPUT') {
+		document.body.scrollTop = 0;
+	}
+});
+
+
+const currentUrl = window.location.href;
+console.log(currentUrl);
--- routes/index.js
+++ routes/index.js
@@ -1,166 +1,151 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const User = require("../models/userModel");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
+const User = require('../models/userModel');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 passport.use(new LocalStrategy(User.authenticate()));
 // passport.use(User.createStrategy());
 
-const { sendmail } = require("../utils/sendmail");
-const Expense = require("../models/expenseModel");
+const { sendmail } = require('../utils/sendmail');
+const Expense = require('../models/expenseModel');
 
-router.get("/", function (req, res, next) {
-    res.render("index", { admin: req.user });
+/*  -------  Login and Signup  ----------------- */
+/*  -------  Login and Signup  ----------------- */
+router.get('/', function (req, res, next) {
+	res.render('index', { admin: req.user });
 });
-
-router.get("/signup", function (req, res, next) {
-    res.render("signup", { admin: req.user });
-});
-
-router.post("/signup", async function (req, res, next) {
-    try {
-        await User.register(
-            { username: req.body.username, email: req.body.email },
-            req.body.password
-        );
-        res.redirect("/signin");
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
-});
-
-router.get("/signin", function (req, res, next) {
-    res.render("signin", { admin: req.user });
-});
-
-router.get("/forget", function (req, res, next) {
-    res.render("forget", { admin: req.user });
-});
-
-router.post("/send-mail", async function (req, res, next) {
-    try {
-        const user = await User.findOne({ email: req.body.email });
-        if (!user)
-            return res.send("User Not Found! <a href='/forget'>Try Again</a>");
-
-        sendmail(user.email, user, res, req);
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
-});
-
-router.post("/forget/:id", async function (req, res, next) {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user)
-            return res.send("User not found! <a href='/forget'>Try Again</a>.");
-
-        if (user.token == req.body.token) {
-            user.token = -1;
-            await user.setPassword(req.body.newpassword);
-            await user.save();
-            res.redirect("/signin");
-        } else {
-            user.token = -1;
-            await user.save();
-            res.send("Invalid Token! <a href='/forget'>Try Again<a/>");
-        }
-    } catch (error) {
-        res.send(error);
-    }
-});
-
 router.post(
-    "/signin",
-    passport.authenticate("local", {
-        successRedirect: "/profile",
-        failureRedirect: "/signin",
-    }),
-    function (req, res, next) {}
+	'/signin',
+	passport.authenticate('local', {
+		successRedirect: '/dashboard',
+		failureRedirect: '/',
+	}),
+	function (req, res, next) {}
 );
-
-router.get("/profile", isLoggedIn, async function (req, res, next) {
-    try {
-        const { expenses } = await req.user.populate("expenses");
-        console.log(req.user, expenses);
-        res.render("profile", { admin: req.user, expenses });
-    } catch (error) {
-        res.send(error);
-    }
+router.post('/signup', async function (req, res, next) {
+	try {
+		await User.register(
+			{ username: req.body.username, email: req.body.email },
+			req.body.password
+		);
+		res.redirect('/');
+	} catch (error) {
+		console.log(error);
+		res.send(error);
+	}
 });
 
-router.get("/reset", isLoggedIn, function (req, res, next) {
-    res.render("reset", { admin: req.user });
+/*  ----------------  Forget Route  & Send Mail ----------------- */
+router.get('/forget', function (req, res, next) {
+	res.render('forgetmail');
 });
 
-router.post("/reset", isLoggedIn, async function (req, res, next) {
-    try {
-        await req.user.changePassword(
-            req.body.oldpassword,
-            req.body.newpassword
-        );
-        await req.user.save();
-        res.redirect("/profile");
-    } catch (error) {
-        res.send(error);
-    }
-});
+router.post('/send-mail', async function (req, res, next) {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		if (!user)
+			return res.send(`<div
 
-router.get("/signout", isLoggedIn, function (req, res, next) {
-    req.logout(() => {
-        res.redirect("/signin");
-    });
-});
+	style="
+	overflow: hidden;
+	font-size:3vw;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	"
+>
+	<h1>User Not Found! <a href="/forget">Try Again</a></h1>
+</div>`);
 
+		sendmail(user.email, user, res, req);
+	} catch (error) {
+		console.log(error);
+		res.send(error);
+	}
+});
+router.post('/forget/:id', async function (req, res, next) {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user)
+			return res.send("User not found! <a href='/forget'>Try Again</a>.");
+
+		if (user.token == req.body.token) {
+			user.token = -1;
+			await user.setPassword(req.body.newpassword);
+			await user.save();
+			res.redirect('/');
+		} else {
+			user.token = -1;
+			await user.save();
+			res.send("Invalid Token! <a href='/forget'>Try Again<a/>");
+		}
+	} catch (error) {
+		res.send(error);
+	}
+});
+/*  ----------------  Forget Route  & Send Mail ----------------- */
+
+/* --------- Reset Password  --------- */
+router.get('/reset', isLoggedIn, function (req, res, next) {
+	res.render('reset', { admin: req.user });
+});
+router.post('/reset', isLoggedIn, async function (req, res, next) {
+	try {
+		await req.user.changePassword(req.body.oldpassword, req.body.newpassword);
+		await req.user.save();
+		res.redirect('/dashboard');
+	} catch (error) {
+		res.send(error);
+	}
+});
+/* --------- Reset Password  --------- */
+
+/* --------- Signout User  --------- */
+router.get('/signout', isLoggedIn, function (req, res, next) {
+	req.logout(() => {
+		res.redirect('/');
+	});
+});
+/* --------- Signout User  --------- */
+
+/* --------- isLoggedIn Middleware --------- */
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.redirect("/signin");
-    }
+	if (req.isAuthenticated()) {
+		next();
+	} else {
+		res.redirect('/');
+	}
 }
-// -----------------------------------------------------------------
-router.get("/createexpense", isLoggedIn, function (req, res, next) {
-    res.render("createexpense", { admin: req.user });
+/* --------- isLoggedIn Middleware --------- */
+
+/* ----------- First Main Dashboard */
+router.get('/dashboard', isLoggedIn, async function (req, res, next) {
+	try {
+		const { expenses } = await req.user.populate('expenses');
+		console.log(req.user, expenses);
+		res.render('dashboard', { admin: req.user, expenses });
+	} catch (error) {
+		res.send(error);
+	}
+});
+/* --------------- Other Pages -------------- */
+router.get('/wallet', function (req, res, next) {
+	res.render('wallet');
 });
 
-router.post("/createexpense", isLoggedIn, async function (req, res, next) {
-    try {
-        const expense = new Expense(req.body);
-        req.user.expenses.push(expense._id);
-        expense.user = req.user._id;
-        await expense.save();
-        await req.user.save();
-        res.redirect("/profile");
-    } catch (error) {
-        res.send(error);
-    }
+router.get('/transaction', function (req, res, next) {
+	res.render('transaction');
+});
+router.get('/profile', function (req, res, next) {
+	res.render('profile');
 });
 
-router.get("/filter", async function (req, res, next) {
-    try {
-        let { expenses } = await req.user.populate("expenses");
-        expenses = expenses.filter((e) => e[req.query.key] == req.query.value);
-        res.render("profile", { admin: req.user, expenses });
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
-});
 
-// router.get("/filter", isLoggedIn, async function (req, res, next) {
-//     try {
-//         let { expenses } = await req.user.populate("expenses");
-//         expenses = expenses.filter((f) => f[req.query.key] == req.query.value);
-//         console.log(expenses);
-//         res.render("profile", { admin: req.user, expenses });
-//     } catch (error) {
-//         console.log(error);
-//         res.send(error);
-//     }
-// });
+router.get('/settings', function (req, res, next) {
+	res.render('settings');
+});
 
 module.exports = router;
