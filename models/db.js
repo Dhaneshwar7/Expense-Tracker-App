52d70a36af9cb6d7d4df57208dbd499a62983285
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2023-12-06 21:11:56 +0530

    render push

--- models/db.js
+++ models/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config({path : "/" + "./.env"})
+
+console.log(require('dotenv').config());
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/ExpenseApp")
+    .connect(process.env.MONGO_URL)
     .then(() => console.log("Database connected!"))
     .catch((err) => console.log(err.message));
