b595e78eb02deca8aae88a7a0259c7ed1eda3888
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2023-12-07 19:35:24 +0530

    mongo url update

--- models/db.js
+++ models/db.js
@@ -4,6 +4,6 @@ require("dotenv").config({path : "/" + "./.env"})
 console.log(require('dotenv').config());
 
 mongoose
-	.connect('process.env.MONGO_URL')
+	.connect(process.env.MONGO_URL)
 	.then(() => console.log('Database connected!'))
 	.catch(err => console.log(err.message));
