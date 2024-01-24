ccdb2720161b293168f1f0dc9761df75e0073e5b
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-01-24 11:55:01 +0530

    expense test 24jan

--- models/db.js
+++ models/db.js
@@ -2,6 +2,6 @@ require('dotenv').config({ path: '/' + './.env' });
 const mongoose = require('mongoose');
 console.log(require('dotenv').config());
 mongoose
-	.connect('mongodb://127.0.0.1:27017/ExpenseApp')
+	.connect(process.env.MONGO_URL)
 	.then(() => console.log('Database connected!'))
 	.catch(err => console.log(err.message));
